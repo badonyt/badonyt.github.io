a947bc51d7e1792b045fe638d0ba42dc3bd8eed7
badonyt <78484256+badonyt@users.noreply.github.com>
2021-04-20 18:43:05 +0100

    first js + test

---
+++ home.js
@@ -0,0 +1,36 @@
+/*   //hellooooooooooooooooooooooooooooooooooooooooooooo
+
+  let streing = 20;
+
+  let hello = "jfnfmskd";
+
+  let add = "adj";
+
+  console.log(streing);
+  console.log(hello); 
+  console.log(add);
+ */
+
+  let name = prompt("whats your name?")
+ 
+  /* if (canhedivided = true){
+  z = number / numbers;
+  console.log(z);
+  }else{
+    console.log("ok " + name + " guess you dont want to divide")
+  } */
+  
+  
+ 
+  
+
+  
+
+const element = document.getElementById('firstjava');
+
+element.textContent = name;
+
+if(name = null){
+    window.location.href = "https://badonyt.github.io/index.html";
+}
+
